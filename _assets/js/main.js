911c7361bf75d26d7dbbb8f86c8f8056ff493de8
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-02-15 17:22:37 +0000

    example content

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,14 @@
 // general js for the project that wouldn't be a reuseable component
+
+
+// a very quick version of auto hide the nav
+// real version should happen on scroll up or mouse position
+
+function toggleNav() {
+  $('.js-hide-site-header').fadeToggle(400);
+  $('.js-hide-site-nav').fadeToggle(400);
+}
+
+toggleNav();
+
+$('.banner').on('click', toggleNav);
