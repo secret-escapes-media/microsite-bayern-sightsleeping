e04b753533bd9fc141896ce984858fa26b29d719
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2018-02-19 16:47:24 +0000

    staging dir

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/collection': '_site/'
+        '/_templates/collection': '_site/'
       }
     }
   });
