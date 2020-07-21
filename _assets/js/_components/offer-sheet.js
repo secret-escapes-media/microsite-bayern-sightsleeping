ffd1f732302375895179b81c4561ccd77275d49c
hamishgray <hamish.gray@secretescapes.com>
2020-07-21 10:15:55 +0100

    fixing dynamic offer integration

--- _assets/js/_components/offer-sheet.js
+++ _assets/js/_components/offer-sheet.js
@@ -30,6 +30,7 @@ $(offerSections).each(function(){
 
     var sortedDeals = deals.sort(function(a,b){ return a[6] > b[6] ?1 :-1 });
 
+    offerSection.empty(); // remove loading icon
     // loop through deals and build HTML for output
     $(deals).each(function(i){
       // check deal "end date" to ensure it exists
@@ -47,43 +48,31 @@ $(offerSections).each(function(){
         var saleURL               = this[9];
         var saleImage             = this[10];
 
-        offerSection.prepend('<div class="col"><div class="offer"><a class="offer__link" href="'+saleURL+'"><span class="btn btn--orange">View Offer</span></a><div class="bg-img bg-img--3-2" style="background-image: url('+saleImage+')"></div><div class="offer__content"><h3 class="title title--xs">'+saleTitle+'</h3><h4 class="offer__location">'+saleLocation+'</h4><div class="offer__description">'+saleDescription+'</div><div class="offer__bottom"><div class="offer__bottom-left"><div class="offer__saved"><div class="offer__saved-label">Save up to&nbsp;</div><div class="offer__saved-percent">'+saleDiscount+'%</div></div><div class="offer__separator"></div><div class="offer__details"><div>From <span class="offer__price">£'+salePrice+'</span></div><div class="text--sm">'+salePriceDescription+'</div><div class="js-offer-expires text--sm" data-expires="'+saleEndDate+'"></div></div></div><div class="offer__bottom-right"><div class="offer__arrow"></div></div></div></div></div></div>');
+        var htmlCol          = '<div class="col"></div>';
+        var htmlOffer        = '<div class="offer depth--sm depth--sm-hover rounded--sm"></div>';
+        var htmlLink         = '<a class="offer__link" href="'+saleURL+'"><span class="btn btn--orange">View Offer</span></a>';
+        var htmlImage        = '<div class="img img--16-9" style="background-image: url('+saleImage+')"><div class="offer__expires js-offer-expires p--sm" data-expires="'+saleEndDate+'"></div></div>';
+        var htmlContentWrap  = '<div class="boxpad--md"></div>';
+        var htmlLocation     = '<h4 class="offer__location">'+saleLocation+'</h4>';
+        var htmlTitle        = '<h3 class="h h--xxs">'+saleTitle+'</h3>';
+        var htmlDescription  = '<div class="offer__description">'+saleDescription+'</div>';
+        var htmlBottom       = '<div class="offer__bottom"></div>';
+        var htmlBottomLeft   = '<div class="offer__bottom-left"><div class="offer__details">From <span class="offer__price">'+salePrice+'</span> '+salePriceDescription+'</div></div>';
+
+        if(saleDiscount > 0){
+          var htmlBottomRight  = '<div class="offer__bottom-right"><div class="offer__discount">-'+saleDiscount+'%</div></div>';
+        }else{
+          var htmlBottomRight = '';
+        }
+
+        var bottom = $(htmlBottom).append(htmlBottomLeft).append(htmlBottomRight);
+        var content = $(htmlContentWrap).append(htmlLocation).append(htmlTitle).append(htmlDescription).append(bottom);
+        var inner = $(htmlOffer).append(htmlLink).append(htmlImage).append(content);
+        var offer = $(htmlCol).append(inner);
+
+        $(offerSection).prepend(offer);
       }// end if
     });// end forloop of deals
   });// end JSON request
 });//end forloop of offerSections
-
-
-
-/*----- EXPANDED OFFER HTML -----
-
-<div class="col">
-  <div class="offer">
-    <a class="offer__link" href="'+saleURL+'"><span class="btn btn--orange">View Offer</span></a>
-    <div class="bg-img bg-img--3-2" style="background-image: url('+saleImage+')"></div>
-    <div class="offer__content">
-      <h3 class="title title--xs">'+saleTitle+'</h3>
-      <h4 class="offer__location">'+saleLocation+'</h4>
-      <div class="offer__description">'+saleDescription+'</div>
-      <div class="offer__bottom">
-        <div class="offer__bottom-left">
-          <div class="offer__saved">
-            <div class="offer__saved-label">Save up to&nbsp;</div>
-            <div class="offer__saved-percent">'+saleDiscount+'%</div>
-          </div>
-          <div class="offer__separator"></div>
-          <div class="offer__details">
-            <div>From <span class="offer__price">£'+salePrice+'</span></div>
-            <div class="text--sm">'+salePriceDescription+'</div>
-            <div class="js-offer-expires text--sm" data-expires="'+saleEndDate+'"></div>
-          </div>
-        </div>
-        <div class="offer__bottom-right">
-          <div class="offer__arrow"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-*/
 
